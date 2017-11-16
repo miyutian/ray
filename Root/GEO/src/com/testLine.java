14f0f6e4d7a49fe0725c013d084e021071a66c72
miyutian <miyutian1@163.com>
2017-11-16 19:37:41 +0800

    update Java code

--- Root/GEO/src/com/testLine.java
+++ Root/GEO/src/com/testLine.java
@@ -39,7 +39,7 @@ public class testLine {
 	            featureSource = (FeatureSource<SimpleFeatureType, SimpleFeature>)shpDataStore.getFeatureSource(typeName);
 	            //FeatureCollection<SimpleFeatureType, SimpleFeature> result = featureSource.getFeatures(); 
 	            
-	            String insertSQL = "insert into 15r3_raw_ntlinks_usa(LINK_ID,F_NODE_ID,T_NODE_ID,LINK_GEOM) values(?,?,?,GeomFromText(?))";
+	            String insertSQL = "insert into nwTable values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,GeomFromText(?),?,?,?,?)";
 	            dbConn = new ConnectDB().connect();
 	            PreparedStatement pstm = dbConn.prepareStatement(insertSQL);
 	            //存储地理对象
@@ -47,27 +47,143 @@ public class testLine {
 	            //System.out.println(result.size());  
 	            FeatureIterator<SimpleFeature> itertor = result.features();  
 	            while(itertor.hasNext()){  
+	            	int paraCount = 1;
 	                SimpleFeature feature = itertor.next();   
-	                int paraCount = 1;
-	                Collection<Property> p = feature.getProperties(); 
-	                Iterator<Property> it = p.iterator(); 
-	                while(it.hasNext()) {  
-	                	
-	                    Property pro = it.next();  
-	                    if (!(pro.getValue() instanceof Point)) {  
-	                        //System.out.println(pro.getName() + " = " + pro.getValue()); 
-	                    	//System.out.println(pro.getName() + " = " + pro.getAttribute("ID")); 
-	                    }  
-	                }
+	    			if ((Integer) feature.getAttribute("FEATTYP") != 4110) {
+	    			    continue;
+	    			}
+	    			
+	    			int func_class = (Integer) feature.getAttribute("FRC");
+	    			switch (func_class) {
+	    			case 0:
+	    			    func_class = 1;
+	    			    break;
+	    			case 1:
+	    			case 2:
+	    			    func_class = 2;
+	    			    break;
+	    			case 3:
+	    			    func_class = 3;
+	    			    break;
+	    			case 4:
+	    			case 5:
+	    			    func_class = 4;
+	    			    break;
+	    			default:
+	    			    func_class = 5;
+	    			}
+	    			
+	    			int lanes = (Integer) feature.getAttribute("LANES");
+	    			int lane_cat = 3;
+	               		switch (lanes) {
+	               		case 0:  		    
+	               		case 1:
+	               		    lane_cat = 1;
+	               		    break;
+	               		case 2:           		
+	               		case 3:
+	               		    lane_cat = 2;
+	               		    break;
+	               		}
+	    		        
+	    			String link_dir = (String) feature.getAttribute("ONEWAY");
+	    			if (link_dir == null) {
+	    			    link_dir = "B";
+	    			} else if (link_dir.equals("N")) {
+	    			    link_dir = "B";//continue;
+	    			} else if (link_dir.equals("FT")) {
+	    			    link_dir = "F";
+	    			} else if (link_dir.equals("TF")) {
+	    			    link_dir = "T";
+	    			}
+	    			
+	    			int fow = (Integer) feature.getAttribute("FOW");
+	    			String fontage = "N";
+	    			if (fow == 11) {
+	    			    fontage = "Y";
+	    			}
+	    			
+	    			int partstruc = (Integer) feature.getAttribute("PARTSTRUC");
+	    			String bridge = "N";
+	    			if (partstruc == 2) {
+	    			    bridge = "Y";
+	    			}
+	    			
+	    			String tunnel = "N";
+	    			if (partstruc == 1) {
+	    			    tunnel = "Y";
+	    			}
+	    			
+	    			int ramp = (Integer) feature.getAttribute("RAMP");
+	    			String rampStr = "N";
+	               		switch (ramp) {
+	               		case 1:
+	               		case 2: 
+	               		    rampStr = "Y";
+	               		    break;
+	               		}
+	               		
+	               		;
+	               		String tollway = "N";
+	               		if (!"".equals(String.valueOf(feature.getAttribute("TOLLRD")))) {
+	               		    tollway = "Y";
+	               		}
+	               		
+	               		String carriage = (String) feature.getAttribute("CARRIAGE");
+	               		String reversible = "N";
+	               		if (carriage != null && (carriage.equals("1") || carriage.equals("2")) && link_dir != null && link_dir.equals("B")) {
+	               		    reversible = "Y";
+	               		}
+	               		
+	               		String expr_lane = "N";           		
+	               		if (carriage != null && carriage.equals("2")) {
+	               		     expr_lane = "Y";
+	               		}
+	               		
+	               		String carpoolrd = "N";
+	               		if (carriage != null && carriage.equals("1")) {
+	               		    carpoolrd = "Y";
+	              		}
+	               		
+	               		int freeway = (Integer) feature.getAttribute("FREEWAY");
+	               		String contracc = "N";
+	               		if (freeway == 1) {
+	               		    contracc = "Y";
+	               		}
+	               
 	                String geometry = feature.getDefaultGeometryProperty().getValue().toString();
 	                System.out.println(feature.getAttribute("ID"));
 	                pstm.setLong(paraCount++, (Long) feature.getAttribute("ID"));
 	                pstm.setLong(paraCount++, (Long) feature.getAttribute("F_JNCTID"));
 	                pstm.setLong(paraCount++, (Long) feature.getAttribute("T_JNCTID"));
-
+	                pstm.setInt(paraCount++, func_class);
+	                pstm.setInt(paraCount++, (Integer)feature.getAttribute("SPEEDCAT"));
+	                pstm.setInt(paraCount++, lanes);
+	                pstm.setInt(paraCount++, lanes);
+	                pstm.setInt(paraCount++, lane_cat);
+	                pstm.setString(paraCount++, link_dir);
+	                pstm.setString(paraCount++, fontage);
+	                pstm.setString(paraCount++, bridge);
+	                pstm.setString(paraCount++, tunnel);
+	                pstm.setString(paraCount++, rampStr);
+	                pstm.setString(paraCount++, tollway);
+	                pstm.setString(paraCount++, "H");
+	                pstm.setString(paraCount++, reversible);
+	                pstm.setString(paraCount++, expr_lane);
+	                pstm.setString(paraCount++, carpoolrd);
+	                pstm.setString(paraCount++, contracc);
+	                pstm.setString(paraCount++, "China");
+	                pstm.setString(paraCount++, "Beijing");
+	                pstm.setString(paraCount++, "110000");
+	                pstm.setString(paraCount++, "");
+	                pstm.setString(paraCount++, "");
 	    			geometry = geometry.replaceFirst("MULTILINESTRING \\(",	"linestring");
 	    			geometry = geometry.substring(0, geometry.length() - 1);
 	    			pstm.setString(paraCount++, geometry.toString());
+	    			pstm.setString(paraCount++, (String)feature.getAttribute("NAME"));
+	    			pstm.setString(paraCount++, (String)feature.getAttribute("NAMELC"));
+	    			pstm.setInt(paraCount++, (Integer)feature.getAttribute("NAMETYP"));
+	    			pstm.setString(paraCount++, (String)feature.getAttribute("RTEDIR"));
 	    			pstm.addBatch();
 	    			batchCount++;
 	    			batchCount++;
