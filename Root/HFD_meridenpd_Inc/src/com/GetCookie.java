254333470de8c76cba355b2db77a4ed94b33ab92
miyutian <miyutian1@163.com>
2017-12-28 18:00:39 +0800

    test2 from jack

--- Root/HFD_meridenpd_Inc/src/com/GetCookie.java
+++ Root/HFD_meridenpd_Inc/src/com/GetCookie.java
@@ -26,81 +26,139 @@ import java.util.Map;
 public class GetCookie {
 	public static void main(String[] args) throws ClientProtocolException, IOException{
 		new GetCookie().Cookie();
+		new GetCookie().GCookie();
 	}
 	public String Cookie(){
+		String Cookie1 = "";
+
 		String Cookie = "";
 		try {
-	        URL googleobj = new URL("http://meridenp2c.com/GoogleMap.aspx");
-	        URLConnection googleconn = googleobj.openConnection();
+
 
 	  	  	String urlPath = new String("http://meridenp2c.com/cad/currentcalls.aspx");
 	  	  	
 	  	  	URL url=new URL(urlPath);
 	  	  	HttpURLConnection httpConn=(HttpURLConnection)url.openConnection();
 	  	          
-	  	          //设置参数
-	  	          //httpConn.setDoOutput(true);     	//需要输出
-	  	  	httpConn.setDoInput(true);      	//需要输入
-	  	  	httpConn.setUseCaches(false);   	//不允许缓存
-	  	  	httpConn.setRequestMethod("GET");   //设置GET方式连接
+	  	          //璁剧疆鍙傛暟
+	  	          //httpConn.setDoOutput(true);     	//闇�杈撳嚭
+	  	  	httpConn.setDoInput(true);      	//闇�杈撳叆
+	  	  	httpConn.setUseCaches(false);   	//涓嶅厑璁哥紦瀛�
+	  	  	httpConn.setRequestMethod("GET");   //璁剧疆GET鏂瑰紡杩炴帴
 	  	          
-	  	          //设置请求属性
+	  	          //璁剧疆璇锋眰灞炴�
 	  	  	httpConn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
 	  	  	httpConn.setRequestProperty("Accept-Encoding", "gzip, deflate");
 	  	  	httpConn.setRequestProperty("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6");
-	  	  	httpConn.setRequestProperty("Connection", "Keep-Alive");// 维持长连接
+	  	  	httpConn.setRequestProperty("Connection", "Keep-Alive");// 缁存寔闀胯繛鎺�
 	  	  	httpConn.setRequestProperty("Charset", "UTF-8");
 	  	  	httpConn.setRequestProperty("Cache-Control", "no-cache");
 	  	  	httpConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
 	  	          
-	  	          //设置浏览器认证
+	  	          //璁剧疆娴忚鍣ㄨ璇�
 	  	          //String userCredentials = "testuser2:33abwe";
 	  	          //String basicAuth = "Basic " + new String(new sun.misc.BASE64Encoder().encode(userCredentials.getBytes()));
 	  	          //httpConn.setRequestProperty("Authorization", basicAuth);
 	  	          
-	  	          //连接,也可以不用明文connect，使用下面的httpConn.getOutputStream()会自动connect
+	  	          //杩炴帴,涔熷彲浠ヤ笉鐢ㄦ槑鏂嘽onnect锛屼娇鐢ㄤ笅闈㈢殑httpConn.getOutputStream()浼氳嚜鍔╟onnect
 	  	  	httpConn.connect();
 	  	  	Map<String, List<String>> map = httpConn.getHeaderFields();
 	  	  	Cookie = map.get("Set-Cookie").toString();
-	  	  	System.out.println(Cookie);
+	  	  	System.out.println("Cookie::"+Cookie);
 
 	  	  	Cookie = Cookie.substring(1, Cookie.indexOf(";"));
-	  	  	System.out.println(Cookie);
+	  	  	System.out.println("Cookie::"+Cookie);
 	  	  	
 	  	  	
-	  	  	String googlePath = new String("http://meridenp2c.com/GoogleMap.aspx");
 	  	  	
-	  	  	URL googleurl=new URL(googlePath);
-	  	  	HttpURLConnection googlehttpConn=(HttpURLConnection)url.openConnection();
+	  	  	
+	  	  String urlPath1 = new String("http://meridenp2c.com/cad/currentcalls.aspx");
+	  	  	
+	  	  	URL url1=new URL(urlPath1);
+	  	  	HttpURLConnection httpConn1=(HttpURLConnection)url1.openConnection();
 	  	          
-	  	          //设置参数
-	  	          //httpConn.setDoOutput(true);     	//需要输出
-		  	 googlehttpConn.setDoInput(true);      	//需要输入
-		  	 googlehttpConn.setUseCaches(false);   	//不允许缓存
-		  	 googlehttpConn.setRequestMethod("GET");   //设置GET方式连接
+	  	          //璁剧疆鍙傛暟
+	  	          //httpConn.setDoOutput(true);     	//闇�杈撳嚭
+	  	  httpConn1.setDoInput(true);      	//闇�杈撳叆
+	  	httpConn1.setUseCaches(false);   	//涓嶅厑璁哥紦瀛�
+	  	httpConn1.setRequestMethod("GET");   //璁剧疆GET鏂瑰紡杩炴帴
 	  	          
-	  	          //设置请求属性
-		  	 googlehttpConn.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-		  	 googlehttpConn.setRequestProperty("Accept-Encoding", "gzip, deflate");
-  		  	 googlehttpConn.setRequestProperty("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6");
- 		  	 googlehttpConn.setRequestProperty("Connection", "Keep-Alive");// 维持长连接
-		  	 googlehttpConn.setRequestProperty("Charset", "UTF-8");
-		  	 googlehttpConn.setRequestProperty("Cache-Control", "no-cache");
-		  	 googlehttpConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
-		  	          
-		  	          //设置浏览器认证
-		  	          //String userCredentials = "testuser2:33abwe";
-		  	          //String basicAuth = "Basic " + new String(new sun.misc.BASE64Encoder().encode(userCredentials.getBytes()));
-		  	          //httpConn.setRequestProperty("Authorization", basicAuth);
-		  	          
-		  	          //连接,也可以不用明文connect，使用下面的httpConn.getOutputStream()会自动connect
-		  	 googlehttpConn.connect();
+	  	          //璁剧疆璇锋眰灞炴�
+	  	httpConn1.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+	  	httpConn1.setRequestProperty("Accept-Encoding", "gzip, deflate");
+	  	httpConn1.setRequestProperty("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6");
+	  	httpConn1.setRequestProperty("Connection", "Keep-Alive");// 缁存寔闀胯繛鎺�
+	  	httpConn1.setRequestProperty("Charset", "UTF-8");
+	  	httpConn1.setRequestProperty("Cache-Control", "no-cache");
+	  	httpConn1.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
+	  	          
+	  	          //璁剧疆娴忚鍣ㄨ璇�
+	  	          //String userCredentials = "testuser2:33abwe";
+	  	          //String basicAuth = "Basic " + new String(new sun.misc.BASE64Encoder().encode(userCredentials.getBytes()));
+	  	          //httpConn.setRequestProperty("Authorization", basicAuth);
+	  	          
+	  	          //杩炴帴,涔熷彲浠ヤ笉鐢ㄦ槑鏂嘽onnect锛屼娇鐢ㄤ笅闈㈢殑httpConn.getOutputStream()浼氳嚜鍔╟onnect
+	  	  	httpConn1.connect();
+	  	  	Map<String, List<String>> map1 = httpConn1.getHeaderFields();
+	  	  	Cookie1 = map1.get("Set-Cookie").toString();
+	  	  	System.out.println("Cookie::"+Cookie1);
+
+	  	  	Cookie1 = Cookie1.substring(1, Cookie1.indexOf(";"));
+	  	  	System.out.println("Cookie::"+Cookie1);
+	  	  	
+	  	  	
 	  	  	
 	  	  	
 		}
 	  	  	catch (Exception e) {
-				// TODO: handle exception
 			}
 	  	  	return Cookie;
 	}
+	public void GCookie() throws IOException{
+		String Cookie1 = "";
+		try {
+
+
+	  	  	String urlPath1 = new String("http://meridenp2c.com/cad/currentcalls.aspx");
+	  	  	
+	  	  	URL url1=new URL(urlPath1);
+	  	  	HttpURLConnection httpConn1=(HttpURLConnection)url1.openConnection();
+	  	          
+	  	          //璁剧疆鍙傛暟
+	  	          //httpConn.setDoOutput(true);     	//闇�杈撳嚭
+	  	  httpConn1.setDoInput(true);      	//闇�杈撳叆
+	  	httpConn1.setUseCaches(false);   	//涓嶅厑璁哥紦瀛�
+	  	httpConn1.setRequestMethod("GET");   //璁剧疆GET鏂瑰紡杩炴帴
+	  	          
+	  	          //璁剧疆璇锋眰灞炴�
+	  	httpConn1.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+	  	httpConn1.setRequestProperty("Accept-Encoding", "gzip, deflate");
+	  	httpConn1.setRequestProperty("Accept-Language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6");
+	  	httpConn1.setRequestProperty("Connection", "Keep-Alive");// 缁存寔闀胯繛鎺�
+	  	httpConn1.setRequestProperty("Charset", "UTF-8");
+	  	httpConn1.setRequestProperty("Cache-Control", "no-cache");
+	  	httpConn1.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
+	  	          
+	  	          //璁剧疆娴忚鍣ㄨ璇�
+	  	          //String userCredentials = "testuser2:33abwe";
+	  	          //String basicAuth = "Basic " + new String(new sun.misc.BASE64Encoder().encode(userCredentials.getBytes()));
+	  	          //httpConn.setRequestProperty("Authorization", basicAuth);
+	  	          
+	  	          //杩炴帴,涔熷彲浠ヤ笉鐢ㄦ槑鏂嘽onnect锛屼娇鐢ㄤ笅闈㈢殑httpConn.getOutputStream()浼氳嚜鍔╟onnect
+	  	  	httpConn1.connect();
+	  	  	Map<String, List<String>> map1 = httpConn1.getHeaderFields();
+	  	  	Cookie1 = map1.get("Set-Cookie").toString();
+	  	  	System.out.println("Cookie::"+Cookie1);
+
+	  	  	Cookie1 = Cookie1.substring(1, Cookie1.indexOf(";"));
+	  	  	System.out.println("Cookie::"+Cookie1);
+	  	  	
+	  	  	
+	  	  	
+	  	  	
+	  	  	
+		}
+	  	  	catch (Exception e) {
+			}
+	}  	
 }
